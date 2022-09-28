6efb7d777cec59c0e18f1f4382b9cd5cb1023c34
Vijayrjm <114559420+Vijayrjm@users.noreply.github.com>
2022-09-28 18:35:34 +0530

    Add files via upload

---
+++ Store.java
@@ -0,0 +1,17 @@
+package com.datatype;
+
+public class Store {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator calci = new Calculator();
+calci.x = 10;
+calci.y = 15;
+calci.add();
+calci.x = 20;
+calci.y = 5;
+calci.sub();
+	}
+	
+
+}

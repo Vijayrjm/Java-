df4588b8015c43832bfc1bea80ca1135dcecfed5
Vijayrjm <114559420+Vijayrjm@users.noreply.github.com>
2022-09-28 18:30:47 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.datatype;
+
+public class Calculator {
+int x;
+int y;
+	void add() {
+		System.out.println("addition of x and y "+"="+ (x+y));
+	}
+		void sub() {
+			System.out.println("subtraction of x and y "+"="+(x-y));
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+

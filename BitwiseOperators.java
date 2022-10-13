888f4867e26a626460c7dcd60594c2af12dc57ad
Vijayrjm <114559420+Vijayrjm@users.noreply.github.com>
2022-10-13 19:07:50 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,18 @@
+package com.selfstudy;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 9,b=12;
+		int c = a&b;
+		int e = a|b;
+		int f = a^b;
+		System.out.println("a&b is= "+ c);
+		System.out.println("a/b is = "+ e);
+		System.out.println("a^b is = "+ f);
+		
+
+	}
+
+}
---
+++ RelationalOperators.java
@@ -0,0 +1,17 @@
+package com.selfstudy;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=15;
+		System.out.println("a is greater than b ="+ (a>b));
+		System.out.println("a is greater than or equal to b ="+ (a>=b));
+		System.out.println("a is less than b ="+ (a<b));
+		System.out.println("a is less than or equal to b ="+ (a>b));
+		System.out.println("a is equal to  b ="+ (a==b));
+		System.out.println("a is not equal to b ="+ (a!=b));
+
+	}
+
+}

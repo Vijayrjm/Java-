6f260c71ec0d4660f2e9845bbd8a078d158cdaba
Vijayrjm <114559420+Vijayrjm@users.noreply.github.com>
2022-10-13 19:10:21 +0530

    Add files via upload

---
+++ Slowtimer.java
@@ -0,0 +1,18 @@
+
+
+public class Slowtimer {
+
+	 void add() {
+		// TODO Auto-generated method stub
+		 
+		 try {
+			 Thread.sleep(5000);//Uncompleted program To be Discussed
+		 
+		 }
+		 catch(Exception e) {
+
+	}
+	
+	 }
+}
+
